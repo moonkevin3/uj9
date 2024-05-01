8e5954d533ffc61e6db67976a157caac0415ccc1
moonkevin3 <168620032+moonkevin3@users.noreply.github.com>
2024-05-01 15:11:46 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+let ip3020 = 0
 basic.forever(function () {
-    basic.showNumber(pins.analogReadPin(AnalogPin.P1))
+    ip3020 = pins.analogReadPin(AnalogPin.P1)
+    if (ip3020 > 800) {
+        pins.analogWritePin(AnalogPin.P2, 45)
+        basic.showIcon(IconNames.Happy)
+    } else {
+        pins.analogWritePin(AnalogPin.P2, 0)
+        basic.showIcon(IconNames.Sad)
+    }
 })
